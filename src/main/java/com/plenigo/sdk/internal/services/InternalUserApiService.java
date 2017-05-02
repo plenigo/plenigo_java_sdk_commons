e53367f7f5837e23f90425da18d98fad2030d5e8
Eyüp Cetinkaya <eyuep.cetinkaya@plenigo.com>
2017-05-02 11:36:37 +0200

    +  fixed the commits

--- src/main/java/com/plenigo/sdk/internal/services/InternalUserApiService.java
+++ src/main/java/com/plenigo/sdk/internal/services/InternalUserApiService.java
@@ -40,7 +40,7 @@ public class InternalUserApiService {
      * @param companyId             The company id
      * @param testMode              The test mode
      * @param productIds            The product ids
-     * @param useExternalCustomerId The customer id.
+     * @param useExternalCustomerId Flag indicating if the customer id parameter is an internal plenigo id or an external customer id
      *
      * @return a boolean indicating if the user has bought the product or not
      *
